264cf2be659c1dbbaadec41373a5c077cc9e4b33
yowcowvg <yoko_ohyama@voyagegroup.com>
2015-08-25 11:45:52 +0900

    add default gitconfig file

--- cookbooks/base/default.rb
+++ cookbooks/base/default.rb
@@ -22,3 +22,9 @@ end
 template "/etc/environment" do
   source "templates/etc/environment.erb"
 end
+
+template "/home/vagrant/.gitconfig" do
+  user "vagrant"
+  source "templates/home/vagrant/gitconfig.erb"
+  not_if "test -e /home/vagrant/.gitconfig"
+end
