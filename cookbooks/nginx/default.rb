cd88b75a274643433f99d4b3bad62cfd754417f9
yowcow <yowcow@gmail.com>
2015-12-17 21:14:07 +0900

    nginx virtual host

--- cookbooks/nginx/default.rb
+++ cookbooks/nginx/default.rb
@@ -2,7 +2,28 @@ package 'nginx' do
   action :install
 end
 
+directory "/var/log/nginx" do
+  action :create
+  mode "0755"
+end
+
+node[:nginx][:hosts].each do |host|
+  directory host[:root] do
+    action :create
+    mode "0777"
+  end
+
+  template "#{node[:nginx][:config_dir]}/#{host[:server_name]}.conf" do
+    action :create
+    mode "0644"
+    source "templates/etc/nginx/sites-enabled/vh-default.conf.erb"
+    variables(
+      server_name: host[:server_name],
+      root: host[:root]
+    )
+  end
+end
+
 service 'nginx' do
   action [:enable, :start]
 end
-
