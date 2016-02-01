6c8ad384a6c9edc409bfab609047623c6998bcd2
yowcow <yowcow@cpan.org>
2016-02-01 11:47:13 +0900

    fix template path

--- cookbooks/base/gitconfig.rb
+++ cookbooks/base/gitconfig.rb
@@ -1,5 +1,5 @@
 template "/home/#{node[:common][:user]}/.gitconfig" do
   user node[:common][:user]
-  source "templates/home/vagrant/gitconfig.erb"
-  not_if "test -e /home/vagrant/.gitconfig"
+  source "templates/home/gitconfig.erb"
+  not_if "test -e /home/#{node[:common][:user]}/.gitconfig"
 end
