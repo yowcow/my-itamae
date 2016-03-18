03ef43becce51b48d965df6d48d946c9f2a80d63
yowcow <yowcow@cpan.org>
2016-03-18 06:56:40 +0000

    add user to docker group

--- cookbooks/docker/default.rb
+++ cookbooks/docker/default.rb
@@ -2,6 +2,6 @@ package "docker.io" do
   action :install
 end
 
-group "docker" do
-  user node[:common][:user]
+execute "docker group" do
+  command "usermod -aG docker #{node[:common][:user]}"
 end
