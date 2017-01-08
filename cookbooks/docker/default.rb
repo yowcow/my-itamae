bd1906ddeead8024438c300bb5d1dd0f02331962
yowcow <yowcow@cpan.org>
2017-01-08 08:46:50 +0000

    remove all

--- cookbooks/docker/default.rb
+++
@@ -1,7 +0,0 @@
-package "docker.io" do
-  action :install
-end
-
-execute "docker group" do
-  command "usermod -aG docker #{node[:common][:user]}"
-end
--- cookbooks/lets-encrypt/default.rb
+++
@@ -1,4 +0,0 @@
-http_request "/usr/local/bin/certbot-auto" do
-  url "https://dl.eff.org/certbot-auto"
-  mode "0755"
-end
