a99dd3891e691022651c3ddbcba554d47ead2a99
yowcow <yowcow@cpan.org>
2017-01-16 11:47:15 +0900

    make distclean only when auto/config.cache exists

--- cookbooks/vim.rb
+++ cookbooks/vim.rb
@@ -3,10 +3,18 @@ git "/tmp/vim" do
   action :sync
 end
 
+execute "Clean previous build" do
+  command <<-CMD
+    cd /tmp/vim/src && \
+    make distclean
+  CMD
+  not_if "test ! -e /tmp/vim/src/auto/config.cache"
+end
+
 execute "Install vim" do
   command <<-CMD
     cd /tmp/vim/src && \
-    make distclean && make && make install
+    make && make install
   CMD
   not_if "test -e /usr/local/bin/vim"
 end
