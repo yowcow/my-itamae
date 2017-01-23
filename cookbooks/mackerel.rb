6e6e100bb50aa5e7d34dd3213c166cad9503b2f0
yowcow <yowcow@cpan.org>
2017-01-23 10:51:29 +0000

    add mackerel

---
+++ cookbooks/mackerel.rb
@@ -0,0 +1,35 @@
+template "/etc/apt/sources.list.d/mackerel.list" do
+  action :create
+  source "mackerel/templates/apt-source.list.erb"
+  mode "0644"
+end
+
+http_request "/tmp/mackerel-gpg.key" do
+  url "https://mackerel.io/assets/files/GPG-KEY-mackerel"
+  not_if "test -e /tmp/mackerel-gpg.key"
+end
+
+execute "Add GPG key" do
+  command <<-CMD
+    apt-key add /tmp/mackerel-gpg.key && \
+    apt-get update -qq
+  CMD
+  not_if "apt-key list | grep mackerel"
+end
+
+%w{
+  mackerel-agent
+  mackerel-agent-plugins
+}.each do |pkg|
+  package pkg
+end
+
+template "/etc/mackerel-agent/mackerel-agent.conf" do
+  action :create
+  source "mackerel/templates/mackerel-agent.conf.erb"
+  mode "0644"
+end
+
+service "mackerel-agent" do
+  action [:enable]
+end
