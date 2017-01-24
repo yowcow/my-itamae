69d5bb93ac71230f2109c4f278ec596441fd6e38
yowcow <yowcow@cpan.org>
2017-01-24 05:50:01 +0000

    fix conf file path

--- cookbooks/mackerel.rb
+++ cookbooks/mackerel.rb
@@ -24,7 +24,7 @@ end
   package pkg
 end
 
-template "/etc/mackerel-agent/mackerel-agent.conf.mine" do
+template "/etc/mackerel-agent/mackerel-agent.conf" do
   action :create
   source "mackerel/templates/mackerel-agent.conf.erb"
   mode "0644"
