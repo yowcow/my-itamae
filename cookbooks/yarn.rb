cbb6c512629fa2d0822251d85ab1838ba77f873a
yowcow <yowcow@cpan.org>
2017-02-07 03:43:18 +0000

    add native yarn

---
+++ cookbooks/yarn.rb
@@ -0,0 +1,20 @@
+template "/etc/apt/sources.list.d/yarn.list" do
+  action :create
+  source "yarn/templates/apt-source.list.erb"
+  mode "0644"
+end
+
+http_request "/tmp/yarn-gpg.key" do
+  url "https://dl.yarnpkg.com/debian/pubkey.gpg"
+  not_if "test -e /tmp/yarn-gpg.key"
+end
+
+execute "Add GPG key" do
+  command <<-CMD
+    apt-key add /tmp/yarn-gpg.key && \
+    apt-get update -qq
+  CMD
+  not_if "apt-key list | grep yarn"
+end
+
+package "yarn"
