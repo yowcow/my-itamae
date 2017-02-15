bed08da75828a397b821916dfba29242d7f0ab6e
yowcow <yowcow@cpan.org>
2017-02-15 05:34:13 +0000

    server token off

--- cookbooks/nginx.rb
+++ cookbooks/nginx.rb
@@ -1,1 +1,12 @@
 package "nginx"
+
+service "nginx" do
+  action [:enable, :reload]
+end
+
+template "/etc/nginx/nginx.conf" do
+  action :create
+  source "nginx/templates/nginx.conf.erb"
+  mode "0644"
+  notifies :reload, "service[nginx]"
+end
