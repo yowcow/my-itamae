011fee95346f14679a60bde2b9fdbd4ae47487ae
yowcow <yowcow@cpan.org>
2017-02-20 09:14:23 +0000

    utf8mb4

--- cookbooks/mysql.rb
+++ cookbooks/mysql.rb
@@ -7,9 +7,13 @@
 end
 
 template "/etc/mysql/mysql.conf.d/mysqld_utf8.cnf" do
-  action :create
-  source "mysql/templates/mysqld_utf8.cnf.erb"
-  mode "0644"
+  action   :create
+  source   "mysql/templates/mysqld_utf8.cnf.erb"
+  mode     "0644"
+  variables(
+    character_set: node[:mysql]["character-set"]
+  )
+  notifies :restart, "service[mysql]"
 end
 
 service "mysql" do
