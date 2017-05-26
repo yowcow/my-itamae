0f28978769d30ed04e8ee80e8196a0f678c936e8
yowcow <yowcow@cpan.org>
2017-05-26 10:41:08 +0000

    lua + vim

---
+++ cookbooks/lua.rb
@@ -0,0 +1,10 @@
+package "liblua5.3-dev"
+
+link "/usr/include/lua" do
+  to "/usr/include/lua5.3"
+end
+
+link "/usr/local/lib/liblua.so" do
+  to "/usr/lib/x86_64-linux-gnu/liblua5.3.so"
+end
+
--- cookbooks/vim.rb
+++ cookbooks/vim.rb
@@ -13,8 +13,12 @@ end
 
 execute "Install vim" do
   command <<-CMD
-    cd /var/tmp/vim/src && \
-    make && make install
+    cd /var/tmp/vim/src \
+    && ./configure \
+      --enable-fail-if-missing \
+      --enable-luainterp \
+      --enable-multibyte \
+    && make && make install
   CMD
   not_if 'cd /var/tmp/vim && test "${$(git tag | tail -n1): -3}" -eq "${$(vim --version | grep "Included patches" | head -n1): -3}"'
 end
