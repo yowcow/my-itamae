9c1bdfcc9e8009bc03e2a8799f9ee6e9fb09b518
yowcow <yowcow@cpan.org>
2017-07-22 13:24:15 +0000

    crontab

--- cookbooks/letsencrypt.rb
+++ cookbooks/letsencrypt.rb
@@ -2,3 +2,9 @@ http_request "/usr/local/bin/certbot-auto" do
   url "https://dl.eff.org/certbot-auto"
   mode "0775"
 end
+
+template "/etc/cron.daily/letsencrypt" do
+  action :create
+  source "letsencrypt/templates/crontab/letsencrypt.erb"
+  model "0755"
+end
