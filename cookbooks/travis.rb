d82a38a38e1b02837695c43d906983becbad4e67
yowcow <yowcow@cpan.org>
2017-10-30 01:06:43 +0000

    gem_resource

--- cookbooks/travis.rb
+++ cookbooks/travis.rb
@@ -1,7 +1,6 @@
 package "ruby-dev"
 
-execute "travis" do
-  command <<-CMD
-    gem install travis -v #{node[:travis][:version]} --no-rdoc --no-ri
-  CMD
+gem_package "travis" do
+  action :install
+  version node[:travis][:version]
 end
