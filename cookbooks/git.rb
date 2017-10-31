af76982dc734ed8bb77a92f8dd5bad9aab408ae3
yowcow <yowcow@cpan.org>
2017-10-31 01:24:02 +0000

    add repository only when apt-key not exists

--- cookbooks/git.rb
+++ cookbooks/git.rb
@@ -1,8 +1,9 @@
-execute 'Add apt repository' do
+execute 'Add git-core apt repository' do
   command <<-CMD
     add-apt-repository -y ppa:git-core/ppa && \
     apt update
   CMD
+  not_if "apt-key list | grep git-core"
 end
 
 package 'git'
