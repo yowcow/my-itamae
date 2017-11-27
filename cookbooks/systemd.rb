7bedd3410b0c3913eea2f8a7291a55cf1f1ef09b
yowcow <yowcow@cpan.org>
2017-11-27 22:05:42 +0900

    systemd

---
+++ cookbooks/systemd.rb
@@ -0,0 +1,18 @@
+%w{
+  logind
+  resolved
+}.each do |d|
+  directory "/etc/systemd/#{d}.conf.d" do
+    action :create
+    mode   "0755"
+  end
+
+  service "systemd-#{d}.service"
+
+  template "/etc/systemd/#{d}.conf.d/#{d}.conf" do
+    action :create
+    source "systemd/templates/#{d}.conf"
+    mode   "0644"
+    notifies :restart, "service[systemd-#{d}.service]"
+  end
+end
