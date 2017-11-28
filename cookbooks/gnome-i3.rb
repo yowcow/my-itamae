208347b8c9399cbf0c7c0be8a21c356dab0b0250
yowcow <yowcow@cpan.org>
2017-11-28 21:59:23 +0900

    not erb

--- cookbooks/gnome-i3.rb
+++ cookbooks/gnome-i3.rb
@@ -1,11 +1,11 @@
 template "/usr/share/xsessions/gnome-i3.desktop" do
   action :create
-  source "gnome-i3/templates/xsessions/gnome-i3.desktop.erb"
+  source "gnome-i3/templates/xsessions/gnome-i3.desktop"
   mode   "0644"
 end
 
 template "/usr/share/gnome-session/sessions/i3.session" do
   action :create
-  source "gnome-i3/templates/gnome-session/sessions/i3.session.erb"
+  source "gnome-i3/templates/gnome-session/sessions/i3.session"
   mode   "0644"
 end
