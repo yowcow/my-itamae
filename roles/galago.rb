f130897eb6280299c58be393162778b7aca70ba1
yowcow <yowcow@cpan.org>
2017-12-16 13:36:55 +0900

    some restricted extras

--- roles/galago.rb
+++ roles/galago.rb
@@ -3,3 +3,11 @@ include_recipe "./base.rb"
 include_recipe "../cookbooks/fonts.rb"
 include_recipe "../cookbooks/docker.rb"
 include_recipe "../cookbooks/docker/compose.rb"
+
+%w{
+  ubuntu-restricted-extras
+  libav-tools
+  ffmpeg
+}.each do |pkg|
+  package pkg
+end
