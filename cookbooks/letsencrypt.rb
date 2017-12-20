ec707483a81e93309fb76f61fb259dca2c8058b6
yowcow <yowcow@cpan.org>
2017-12-20 11:23:57 +0000

    curl certbot-auto

--- cookbooks/letsencrypt.rb
+++ cookbooks/letsencrypt.rb
@@ -1,6 +1,8 @@
-http_request "/usr/local/bin/certbot-auto" do
-  url "https://dl.eff.org/certbot-auto"
-  mode "0775"
+execute "Install certbot-auto" do
+  command <<-CMD
+    curl -L https://dl.eff.org/certbot-auto -o /usr/local/bin/certbot-auto
+    chmod 775 /usr/local/bin/certbot-auto
+  CMD
 end
 
 template "/etc/cron.daily/letsencrypt" do
