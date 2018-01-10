378b39023362e1a661b9a31f6f26c42ace4197ed
yowcow <yowcow@cpan.org>
2018-01-10 12:13:18 +0900

    some essential things

--- cookbooks/common/essential.rb
+++ cookbooks/common/essential.rb
@@ -9,10 +9,19 @@
   libevent-dev
   libicu-dev
   libssl-dev
+  linux-headers-generic
+  linux-image-generic
   pkg-config
   software-properties-common
+  tzdata
 }.each do |pkg|
   package pkg do
     action :install
   end
+end
+
+execute "set /etc/timezone" do
+  command <<-CMD
+    echo "Asia/Tokyo" > /etc/timezone
+  CMD
 end
