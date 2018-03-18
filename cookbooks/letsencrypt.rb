620d6a3d4701584835cfedaffdbbfa34f9bf8ff2
yowcow <yowcow@cpan.org>
2018-03-18 16:03:07 +0900

    letsencrypt to rotate log daily, for 14 days

--- cookbooks/letsencrypt.rb
+++ cookbooks/letsencrypt.rb
@@ -10,3 +10,9 @@ template "/etc/cron.daily/letsencrypt" do
   source "letsencrypt/templates/crontab/letsencrypt.erb"
   mode "0775"
 end
+
+template "/etc/logrotate.d/letsencrypt" do
+  action :create
+  source "letsencrypt/templates/logrotate/letsencrypt.erb"
+  mode "0644"
+end
