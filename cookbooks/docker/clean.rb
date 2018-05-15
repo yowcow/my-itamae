8485c94acc689164aa81c7ab5c7ef56d77edd257
yowcow <yowcow@cpan.org>
2018-05-15 10:45:04 +0900

    fix docker-clean

--- cookbooks/docker/clean.rb
+++ cookbooks/docker/clean.rb
@@ -1,16 +1,23 @@
-version = node[:docker][:compose][:version]
+version = node[:docker][:clean][:version]
+archive = "docker-clean-v#{version}.tar.gz"
+url = "https://github.com/ZZROTDesign/docker-clean/archive/v#{version}.tar.gz"
+dir = "/usr/local/docker-clean-#{version}"
 
-directory "/usr/local/docker-clean-#{version}/bin" do
-  action :create
+http_request "/tmp/#{archive}" do
+  url url
+  mode "0644"
+  not_if "test -e /tmp/#{archive} || test -d #{dir}"
 end
 
-http_request "/usr/local/docker-clean-#{version}/bin/docker-clean" do
-  url "https://raw.githubusercontent.com/ZZROTDesign/docker-clean/v2.0.4/docker-clean"
-  mode "0755"
-  not_if "test -e /usr/local/docker-clean-#{version}/bin/docker-clean"
+execute "Unarchive /tmp/#{archive}" do
+  command <<-CMD
+    tar xzf /tmp/#{archive} -C /usr/local
+    chmod 755 #{dir}
+  CMD
+  not_if "test -d #{dir}"
 end
 
 link "/usr/local/bin/docker-clean" do
-  to "/usr/local/docker-clean-#{version}/bin/docker-clean"
+  to "/usr/local/docker-clean-#{version}/docker-clean"
   force true
 end
