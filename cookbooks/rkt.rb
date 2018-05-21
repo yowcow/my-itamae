168f27f6a02b9eb694805bb118803e0cbb05d49b
yowcow <yowcow@cpan.org>
2018-05-21 20:57:51 +0900

    rkt is in ubuntu repository

--- cookbooks/rkt.rb
+++ cookbooks/rkt.rb
@@ -1,17 +1,19 @@
-version = node[:rkt][:version]
-deb     = "rkt_#{version}-1_amd64.deb"
-baseurl = "https://github.com/rkt/rkt/releases/download/v#{version}"
-current = `(which rkt && rkt version) | grep "rkt Version" | cut -d " " -f 3 | tr -d "\n"`
+package 'rkt'
 
-if current != version then
-  http_request "/tmp/#{deb}" do
-    url "#{baseurl}/#{deb}"
-    not_if "test -f /tmp/#{deb}"
-  end
-
-  execute "install /tmp/#{deb}" do
-    command <<-CMD
-      dpkg -i /tmp/#{deb}
-    CMD
-  end
-end
+#version = node[:rkt][:version]
+#deb     = "rkt_#{version}-1_amd64.deb"
+#baseurl = "https://github.com/rkt/rkt/releases/download/v#{version}"
+#current = `(which rkt && rkt version) | grep "rkt Version" | cut -d " " -f 3 | tr -d "\n"`
+#
+#if current != version then
+#  http_request "/tmp/#{deb}" do
+#    url "#{baseurl}/#{deb}"
+#    not_if "test -f /tmp/#{deb}"
+#  end
+#
+#  execute "install /tmp/#{deb}" do
+#    command <<-CMD
+#      dpkg -i /tmp/#{deb}
+#    CMD
+#  end
+#end
