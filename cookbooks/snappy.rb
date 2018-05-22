c4f16fdaed97d42e4c8d734aa926755128137ff5
yowcow <yowcow@cpan.org>
2018-05-22 19:51:26 +0900

    improve snappy

--- cookbooks/snappy.rb
+++ cookbooks/snappy.rb
@@ -1,4 +1,19 @@
-include_recipe "./snappy/awscli.rb"
+node[:snappy].each do |s|
+  cmd = ['snap', 'install', s[:snap]]
+  cmd << '--classic' if s[:classic]
+
+  execute "Install snap: #{s[:snap]}" do
+    command cmd.join(' ')
+    not_if "snap list | grep #{s[:snap]}"
+  end
+end
+
+file '/usr/local/etc/profile.d/snappy.sh' do
+  content <<-CONTENT
+PATH=/snap/bin:$PATH
+  CONTENT
+  mode "0644"
+end
 
 execute "Refresh all snaps" do
   command <<-CMD
