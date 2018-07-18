cacdd59b4bdaf50452f22627da1142971e5e8747
yowcow <yowcow@cpan.org>
2018-07-18 19:58:55 +0900

    update batch command

--- cookbooks/letsencrypt.rb
+++ cookbooks/letsencrypt.rb
@@ -1,13 +1,13 @@
 package 'certbot'
 
-#template "/etc/cron.daily/letsencrypt" do
-#  action :create
-#  source "letsencrypt/templates/crontab/letsencrypt.erb"
-#  mode "0775"
-#end
-#
-#template "/etc/logrotate.d/letsencrypt" do
-#  action :create
-#  source "letsencrypt/templates/logrotate/letsencrypt.erb"
-#  mode "0644"
-#end
+template "/etc/cron.daily/letsencrypt" do
+  action :create
+  source "letsencrypt/templates/crontab/letsencrypt.erb"
+  mode "0775"
+end
+
+template "/etc/logrotate.d/letsencrypt" do
+  action :create
+  source "letsencrypt/templates/logrotate/letsencrypt.erb"
+  mode "0644"
+end
