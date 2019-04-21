91075a3c0648a480656997a0282b17419f5c8977
yowcow <yowcow@cpan.org>
2019-04-21 15:40:55 +0900

    centralize user/group mgmt

--- cookbooks/common/user.rb
+++ cookbooks/common/user.rb
@@ -1,3 +1,16 @@
+group "znc" do
+  gid 2001
+end
+
+user "znc" do
+  uid 2001
+  gid 2001
+  system_user true
+  shell "/bin/false"
+  home "/var/lib/znc"
+  create_home true
+end
+
 group "www-user" do
   gid 1100
 end
--- cookbooks/znc.rb
+++ cookbooks/znc.rb
@@ -1,18 +1,5 @@
 package "znc"
 
-group "znc" do
-  gid 2001
-end
-
-user "znc" do
-  uid 2001
-  gid 2001
-  system_user true
-  shell "/bin/false"
-  home "/var/lib/znc"
-  create_home true
-end
-
 template "/etc/systemd/system/znc.service" do
   action :create
   source "znc/templates/systemd/znc.service.erb"
