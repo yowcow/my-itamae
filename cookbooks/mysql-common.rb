e0e78c03bc5ce15eea9310a62aac33e71aa319ba
yowcow <yowcow@cpan.org>
2019-04-25 16:17:35 +0900

    do not place client.cnf

--- cookbooks/mysql-common.rb
+++ cookbooks/mysql-common.rb
@@ -5,10 +5,8 @@
   package pkg
 end
 
-charset = "utf8mb4"
-
-template "/etc/mysql/conf.d/client.cnf" do
-  action   :create
-  source   "mysql-common/templates/client.cnf.erb"
-  mode     "0644"
-end
+#template "/etc/mysql/conf.d/client.cnf" do
+#  action   :create
+#  source   "mysql-common/templates/client.cnf.erb"
+#  mode     "0644"
+#end
