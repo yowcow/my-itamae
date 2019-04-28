4460986b484b17c8c184a40ce3151be6d39ff2ad
yowcow <yowcow@cpan.org>
2019-04-29 00:16:57 +0900

    update

--- roles/galago.rb
+++ roles/galago.rb
@@ -5,14 +5,11 @@ include_recipe "../cookbooks/docker.rb"
 include_recipe "../cookbooks/docker/clean.rb"
 include_recipe "../cookbooks/docker/compose.rb"
 include_recipe "../cookbooks/gogh.rb"
-include_recipe "../cookbooks/google-cloud-sdk.rb"
-#include_recipe "../cookbooks/protobuf.rb"
-#include_recipe "../cookbooks/grpc.rb" # depends on protobuf
+##include_recipe "../cookbooks/protobuf.rb"
+##include_recipe "../cookbooks/grpc.rb" # depends on protobuf
 include_recipe "../cookbooks/percona.rb"
 include_recipe "../cookbooks/rkt.rb"
 include_recipe "../cookbooks/snappy.rb"
-include_recipe "../cookbooks/vagrant.rb"
-include_recipe "../cookbooks/virtualbox.rb"
 include_recipe "../cookbooks/weechat.rb"
 
 %w{
