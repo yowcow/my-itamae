3c88d6d18abeabb16be8882bb46257a4950793a5
yowcow <yowcow@cpan.org>
2019-06-03 15:08:41 +0900

    install app image

--- cookbooks/neovim.rb
+++ cookbooks/neovim.rb
@@ -2,77 +2,72 @@ include_recipe "./ctags.rb"
 include_recipe "./lua.rb"
 include_recipe "./python.rb"
 
-%w{
-  autoconf
-  automake
-  cmake
-  gettext
-  libtool
-  libtool-bin
-  pkg-config
-}.each do |pkg|
-  package pkg
-end
-
 version = node[:neovim][:version]
 
-archive = "v#{version}.tar.gz"
-url = "https://github.com/neovim/neovim/archive/#{archive}"
-dir = "/usr/local/neovim-v#{version}"
+#archive = "v#{version}.tar.gz"
+#url = "https://github.com/neovim/neovim/archive/#{archive}"
+#dir = "/usr/local/neovim-v#{version}"
+#
+#%w{
+#  autoconf
+#  automake
+#  cmake
+#  gettext
+#  libtool
+#  libtool-bin
+#  pkg-config
+#}.each do |pkg|
+#  package pkg
+#end
+#
+#if not File.exists?("#{dir}/bin/nvim") then
+#  http_request "/tmp/#{archive}" do
+#    url url
+#    not_if "test -f /tmp/#{archive}"
+#  end
+#
+#  execute "extract #{archive}" do
+#    command <<-CMD
+#      tar xzf /tmp/#{archive} -C /tmp
+#    CMD
+#    not_if "test -d /tmp/neovim-#{version}"
+#  end
+#
+#  template "/tmp/neovim-#{version}/Makefile.patch" do
+#    source "neovim/patch/Makefile.patch"
+#    mode "0644"
+#  end
+#
+#  execute "patch Makefile" do
+#    command <<-CMD
+#      cd /tmp/neovim-#{version} && \
+#      (patch -u -s -f < Makefile.patch) || true
+#    CMD
+#  end
+#
+#  execute "install neovim v#{version}" do
+#    command <<-CMD
+#      cd /tmp/neovim-#{version} && \
+#      make CMAKE_INSTALL_PREFIX=#{dir} CMAKE_BUILD_TYPE=Release && \
+#      make CMAKE_INSTALL_PREFIX=#{dir} CMAKE_BUILD_TYPE=Release install
+#    CMD
+#  end
+#end
 
-if not File.exists?("#{dir}/bin/nvim") then
-  http_request "/tmp/#{archive}" do
-    url url
-    not_if "test -f /tmp/#{archive}"
-  end
+url = "https://github.com/neovim/neovim/releases/download/v#{version}/nvim.appimage"
+dir = "/usr/local/neovim-v#{version}/bin"
 
-  execute "extract #{archive}" do
-    command <<-CMD
-      tar xzf /tmp/#{archive} -C /tmp
-    CMD
-    not_if "test -d /tmp/neovim-#{version}"
-  end
+directory dir do
+  action :create
+end
 
-  template "/tmp/neovim-#{version}/Makefile.patch" do
-    source "neovim/patch/Makefile.patch"
-    mode "0644"
-  end
-
-  execute "patch Makefile" do
-    command <<-CMD
-      cd /tmp/neovim-#{version} && \
-      (patch -u -s -f < Makefile.patch) || true
-    CMD
-  end
-
-  execute "install neovim v#{version}" do
-    command <<-CMD
-      cd /tmp/neovim-#{version} && \
-      make CMAKE_INSTALL_PREFIX=#{dir} CMAKE_BUILD_TYPE=Release && \
-      make CMAKE_INSTALL_PREFIX=#{dir} CMAKE_BUILD_TYPE=Release install
-    CMD
-  end
+http_request "#{dir}/nvim" do
+  url url
+  mode "0755"
+  not_if "test -f #{dir}/nvim"
 end
 
 link "/usr/local/bin/nvim" do
-  to "#{dir}/bin/nvim"
+  to "#{dir}/nvim"
   force true
 end
-
-#url = "https://github.com/neovim/neovim/releases/download/v#{version}/nvim.appimage"
-#dir = "/usr/local/neovim-v#{version}/bin"
-#
-#directory dir do
-#  action :create
-#end
-#
-#http_request "#{dir}/nvim" do
-#  url url
-#  mode "0755"
-#  not_if "test -f #{dir}/nvim"
-#end
-#
-#link "/usr/local/bin/nvim" do
-#  to "#{dir}/nvim"
-#  force true
-#end
