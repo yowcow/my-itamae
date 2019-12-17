3fd219a476a943f4ab609adb90655c2619e74adb
yowcow <yowcow@cpan.org>
2019-12-17 19:54:50 +0900

    update erlang

--- cookbooks/erlang/rebar3.rb
+++ cookbooks/erlang/rebar3.rb
@@ -1,18 +1,17 @@
 version = node[:erlang][:rebar3][:version]
 
-target = "/usr/local/rebar3-#{version}/bin"
+version_file = "/usr/local/src/erlang-rebar3-version"
 
-directory target do
-  action :create
-end
+current_version = File.exists?(version_file) ? File.open(version_file).read.chomp : ""
 
-http_request "#{target}/rebar3" do
-  url "https://github.com/erlang/rebar3/releases/download/#{version}/rebar3"
-  mode "0755"
-  not_if "test -e #{target}/rebar3"
-end
+if current_version != version then
+  http_request "/usr/local/bin/rebar3" do
+    url "https://github.com/erlang/rebar3/releases/download/#{version}/rebar3"
+    mode "0755"
+  end
 
-link "/usr/local/bin/rebar3" do
-  to "#{target}/rebar3"
-  force true
+  file version_file do
+    content version
+    mode "0644"
+  end
 end
