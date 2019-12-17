e3015bd9980187c82570357d257c6733a67d7445
yowcow <yowcow@cpan.org>
2019-12-17 19:32:00 +0900

    update alp

--- cookbooks/alp.rb
+++ cookbooks/alp.rb
@@ -1,26 +1,32 @@
 version = node[:alp][:version]
-archive = "alp_#{version}_linux_amd64.zip"
-url = "https://github.com/tkuchiki/alp/releases/download/v#{version}/alp_linux_amd64.zip"
-targetdir = "/usr/local/alp-#{version}"
 
-http_request "/tmp/#{archive}" do
-  url url
-  not_if "test -f #{targetdir}/alp || test -f /tmp/#{archive}"
-end
+archive      = "alp_#{version}_linux_amd64.zip"
+url          = "https://github.com/tkuchiki/alp/releases/download/v#{version}/alp_linux_amd64.zip"
+version_file = "/usr/local/src/alp-version"
 
-directory targetdir do
-  mode "0755"
-  action :create
-end
+current_version = File.exists?(version_file) ? File.open(version_file).read.chomp : ""
 
-execute "Unarchive /tmp/#{archive}" do
-  command <<-CMD
-    unzip /tmp/#{archive} -d #{targetdir}
-  CMD
-  not_if "test -f #{targetdir}/alp"
-end
+if current_version != version then
+  http_request "/tmp/#{archive}" do
+    url url
+    not_if "test -f /tmp/#{archive}"
+  end
 
-link "/usr/local/bin/alp" do
-  to "#{targetdir}/alp"
-  force true
+  execute "Unarchive /tmp/#{archive}" do
+    command <<-CMD
+      unzip /tmp/#{archive} -d /tmp && \
+      mv /tmp/alp /usr/local/bin/alp
+    CMD
+  end
+
+  file "/usr/local/bin/alp" do
+    owner "root"
+    group "root"
+    mode "0755"
+  end
+
+  file version_file do
+    content version
+    mode "0644"
+  end
 end
