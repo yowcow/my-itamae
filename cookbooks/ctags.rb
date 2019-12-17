ed3a9b72a52a3f54ed738534ef0cb230a1afc1f6
yowcow <yowcow@cpan.org>
2019-12-17 19:42:19 +0900

    update ctags

--- cookbooks/ctags.rb
+++ cookbooks/ctags.rb
@@ -1,35 +1,34 @@
 version = node[:ctags][:version]
 
-archive = "ctags-#{version}.tar.gz"
-url     = "http://prdownloads.sourceforge.net/ctags/#{archive}"
+archive      = "ctags-#{version}.tar.gz"
+url          = "http://prdownloads.sourceforge.net/ctags/#{archive}"
+version_file = "/usr/local/src/ctags-version"
 
-target  = "/usr/local/ctags-#{version}"
-profile = "/usr/local/etc/profile.d/ctags.sh"
+current_version = File.exists?(version_file) ? File.open(version_file).read.chomp : ""
 
-http_request "/tmp/#{archive}" do
-  url url
-  not_if "test -d #{target} || test -f /tmp/#{archive}"
-end
+if current_version != version then
+  http_request "/tmp/#{archive}" do
+    url url
+    not_if "test -f /tmp/#{archive}"
+  end
 
-execute "Unarchive #{archive}" do
-  command <<-CMD
-    tar xzf /tmp/ctags-#{version}.tar.gz -C /tmp
-  CMD
-  not_if "test -d #{target} || test -d /tmp/ctags-#{version}"
-end
+  execute "Unarchive #{archive}" do
+    command <<-CMD
+      tar xzf /tmp/ctags-#{version}.tar.gz -C /tmp
+    CMD
+    not_if "test -d /tmp/ctags-#{version}"
+  end
 
-execute "Install #{target}" do
-  command <<-CMD
-    cd /tmp/ctags-#{version} && \
-    ./configure --prefix #{target} && \
-    make && make install
-  CMD
-  not_if "test -d #{target}"
-end
+  execute "Install ctags" do
+    command <<-CMD
+      cd /tmp/ctags-#{version} && \
+      ./configure --prefix /usr/local && \
+      make && make install
+    CMD
+  end
 
-file profile do
-  content <<-CONTENT
-PATH=#{target}/bin:$PATH
-  CONTENT
-  mode "0644"
+  file version_file do
+    content version
+    mode "0644"
+  end
 end
