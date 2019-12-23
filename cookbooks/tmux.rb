8fa829c45586dcb95d19c8a5667f275a89ae5e60
yowcow <yowcow@cpan.org>
2019-12-23 11:05:33 +0900

    update

--- cookbooks/tmux.rb
+++ cookbooks/tmux.rb
@@ -9,34 +9,33 @@ version = node[:tmux][:version]
 
 archive = "tmux-#{version}.tar.gz"
 url     = "https://github.com/tmux/tmux/releases/download/#{version}/#{archive}"
+target  = "/usr/local"
+version_file = "/usr/local/src/tmux-version"
 
-target  = "/usr/local/tmux-#{version}"
-profile = "/usr/local/etc/profile.d/tmux.sh"
+current_version = File.exists?(version_file) ? File.open(version_file).read.chomp : ""
 
-http_request "/tmp/#{archive}" do
-  url url
-  not_if "test -d #{target} || test -f /tmp/#{archive}"
-end
+if current_version != version then
+  http_request "/tmp/#{archive}" do
+    url url
+  end
 
-execute "Unarchive #{archive}" do
-  command <<-CMD
-    tar zxf /tmp/#{archive} -C /tmp
-  CMD
-  not_if "test -d #{target}"
-end
+  execute "Unarchive #{archive}" do
+    command <<-CMD
+      tar zxf /tmp/#{archive} -C /tmp
+    CMD
+  end
 
-execute "Install to #{target}" do
-  command <<-CMD
-    cd /tmp/tmux-#{version} && \
-    ./configure --prefix #{target} && \
-    make && make install
-  CMD
-  not_if "test -d #{target}"
-end
+  execute "Install to #{target}" do
+    command <<-CMD
+      cd /tmp/tmux-#{version} && \
+      ./configure --prefix #{target} && \
+      make && make install
+    CMD
+    not_if "test -d #{target}"
+  end
 
-file profile do
-  content <<-CONTENT
-PATH=#{target}/bin:$PATH
-  CONTENT
-  mode "0644"
+  file version_file do
+    content version
+    mode "0644"
+  end
 end
--- cookbooks/vim.rb
+++ cookbooks/vim.rb
@@ -6,44 +6,42 @@ version = node[:vim][:version]
 
 archive = "vim-#{version}.tar.gz"
 url     = "https://github.com/vim/vim/archive/v#{version}.tar.gz"
+target  = "/usr/local"
+version_file = "/usr/local/src/vim-version"
 
-target  = "/usr/local/vim-#{node[:vim][:version]}"
-profile = "/usr/local/etc/profile.d/vim.sh"
+current_version = File.exists?(version_file) ? File.open(version_file).read.chomp : ""
 
-http_request "/tmp/#{archive}" do
-  url url
-  not_if "test -d #{target} || test -f /tmp/#{archive}"
-end
+if current_version != version then
+  http_request "/tmp/#{archive}" do
+    url url
+  end
 
-execute "Extract #{archive}" do
-  command <<-CMD
-    tar xzf /tmp/#{archive} -C /tmp
-  CMD
-  not_if "test -d #{target}"
-end
+  execute "Extract #{archive}" do
+    command <<-CMD
+      tar xzf /tmp/#{archive} -C /tmp
+    CMD
+  end
 
-execute "Install to #{target}" do
-  command <<-CMD
-    cd /tmp/vim-#{version}/src \
-    && ./configure \
-      --prefix=#{target} \
-      --enable-fail-if-missing \
-      --enable-luainterp \
-      --with-lua-prefix=/usr/local \
-      --enable-multibyte \
-      --enable-pythoninterp=dynamic \
-      --with-python-config-dir=/usr/lib/python2.7/config-x86_64-linux-gnu \
-      --enable-python3interp=dynamic \
-      --with-python3-config-dir=/usr/lib/python3.6/config-3.6m-x86_64-linux-gnu \
-      --enable-terminal \
-    && make && make install
-  CMD
-  not_if "test -d #{target}"
-end
+  execute "Install to #{target}" do
+    command <<-CMD
+      cd /tmp/vim-#{version}/src \
+      && ./configure \
+        --prefix=#{target} \
+        --enable-fail-if-missing \
+        --enable-luainterp \
+        --with-lua-prefix=/usr/local \
+        --enable-multibyte \
+        --enable-pythoninterp=dynamic \
+        --with-python-config-dir=/usr/lib/python2.7/config-x86_64-linux-gnu \
+        --enable-python3interp=dynamic \
+        --with-python3-config-dir=/usr/lib/python3.6/config-3.6m-x86_64-linux-gnu \
+        --enable-terminal \
+      && make && make install
+    CMD
+  end
 
-file profile do
-  content <<-CONTENT
-PATH=#{target}/bin:$PATH
-  CONTENT
-  mode "0644"
+  file version_file do
+    content version
+    mode "0644"
+  end
 end
