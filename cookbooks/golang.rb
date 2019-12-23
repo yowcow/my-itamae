b6059d6521415fa85852c43acab8245b5a49d4f6
yowcow <yowcow@cpan.org>
2019-12-23 10:11:05 +0900

    updatea golang

--- cookbooks/golang.rb
+++ cookbooks/golang.rb
@@ -2,28 +2,31 @@ version = node[:golang][:version]
 
 archive = "go#{version}.linux-amd64.tar.gz"
 url     = "https://storage.googleapis.com/golang/#{archive}"
+version_file = "/usr/local/src/golang-version"
 
-target  = "/usr/local/go-#{version}"
-profile = "/usr/local/etc/profile.d/golangrc.sh"
+current_version = File.exists?(version_file) ? File.open(version_file).read.chomp : ""
 
-http_request "/tmp/#{archive}" do
-  url url
-  not_if "test -d #{target} || test -f /tmp/#{archive}"
+if current_version != version then
+  http_request "/tmp/#{archive}" do
+    url url
+    not_if "test -f /tmp/#{archive}"
+  end
+
+  execute "Install to golang-#{version}" do
+    command <<-CMD
+      tar xzf /tmp/#{archive} -C /usr/local
+    CMD
+  end
+
+  file version_file do
+    content version
+    mode "0644"
+  end
 end
 
-execute "Install to #{target}" do
-  command <<-CMD
-    tar xzf /tmp/#{archive} -C /tmp && \
-    mv /tmp/go #{target}
-  CMD
-  not_if "test -d #{target}"
-end
-
-file profile do
-  content <<-CONTENT
-export GOROOT=#{target}
-export GOPATH=$HOME/go
-PATH=$GOPATH/bin:$GOROOT/bin:$PATH
-  CONTENT
+file "/etc/profile.d/golang.sh" do
+  content <<-END
+PATH=/usr/local/go/bin:$PATH
+  END
   mode "0644"
 end
