77c6840c326604540186c7fa55e12755a887f220
yowcow <yowcow@cpan.org>
2019-12-25 10:53:38 +0900

    ruby

---
+++ cookbooks/ruby.rb
@@ -0,0 +1,18 @@
+%w{
+  ruby
+}.each do |pkg|
+  package pkg
+end
+
+execute "update gem" do
+  command <<-CMD
+    gem update --system && \
+    gem update
+  CMD
+end
+
+execute "gem install" do
+  command <<-CMD
+    gem i neovim bundler
+  CMD
+end
