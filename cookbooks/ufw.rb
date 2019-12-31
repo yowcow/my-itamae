2e4024e51e57e418d8acfa7a127df88ba71964f8
yowcow <yowcow@cpan.org>
2019-12-31 16:07:32 +0900

    improve ufw

--- cookbooks/ufw.rb
+++ cookbooks/ufw.rb
@@ -1,12 +1,32 @@
-execute "configure ufw" do
-  command <<-CMD
-    ufw default DENY
-    ufw allow 22
-    ufw allow 80
-    ufw allow 443
-    ufw allow 6699
-    ufw allow 6697
-    ufw reload
-    ufw enable
-  CMD
+env = ENV["ENVNAME"]
+
+if not node[:ufw][env][:enable] then
+  execute "disable ufw" do
+    command <<-CMD
+      ufw disable
+    CMD
+  end
+else
+  default = node[:ufw][env][:default]
+  allow = node[:ufw][env][:allow]
+
+  execute "ufw default to #{default}" do
+    command <<-CMD
+      ufw default #{default}
+    CMD
+  end
+
+  allow.each do |port|
+    execute "ufw to allow #{port}" do
+      command <<-CMD
+        ufw allow #{port}
+      CMD
+    end
+  end
+
+  execute "enable ufw" do
+    command <<-CMD
+      ufw enable
+    CMD
+  end
 end
