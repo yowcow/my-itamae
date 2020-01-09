894a727e9cb2e6e684d63615305a1544ea11e421
yowcow <yowcow@cpan.org>
2020-01-09 19:12:33 +0900

    add openjdk

--- cookbooks/elixir.rb
+++ cookbooks/elixir.rb
@@ -1,9 +1,9 @@
 version      = node[:elixir][:version]
 version_file = "/usr/local/src/elixir-version"
 
-url     = "https://github.com/elixir-lang/elixir/archive/v#{version}.tar.gz"
+prefix  = "/usr/local/elixir"
 archive = "elixir-#{version}.tar.gz"
-target  = "/usr/local/elixir"
+url     = "https://github.com/elixir-lang/elixir/archive/v#{version}.tar.gz"
 profile = "/etc/profile.d/elixir.sh"
 
 current_version = File.exists?(version_file) ? File.open(version_file).read.chomp : ""
@@ -11,7 +11,7 @@ current_version = File.exists?(version_file) ? File.open(version_file).read.chom
 if current_version != version then
   execute "Uninstall if any exists" do
     command <<-CMD
-      rm -rf #{target}
+      rm -rf #{prefix}
     CMD
   end
 
@@ -23,13 +23,13 @@ if current_version != version then
   execute "Extract #{archive}" do
     command <<-CMD
       cd /tmp && tar xzf #{archive} && \
-      mv elixir-#{version} #{target}
+      mv elixir-#{version} #{prefix}
     CMD
   end
 
-  execute "Build #{target}" do
+  execute "Build #{prefix}" do
     command <<-CMD
-      LANG=en_US.UTF-8 make -C #{target} clean test
+      LANG=en_US.UTF-8 make -C #{prefix} clean test
     CMD
   end
 
@@ -41,7 +41,7 @@ end
 
 file profile do
   content <<-END
-PATH=/usr/local/elixir/bin:$PATH
+PATH=#{prefix}/bin:$PATH
   END
   mode "0644"
 end
