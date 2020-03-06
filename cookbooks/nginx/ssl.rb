e3e88ec838ce58cd4843b65ac09a68ffc78f4eab
yowcow <yowcow@cpan.org>
2020-03-07 00:35:25 +0900

    enhanced security

--- cookbooks/nginx/ssl.rb
+++ cookbooks/nginx/ssl.rb
@@ -2,6 +2,12 @@ service "nginx" do
   action :reload
 end
 
+http_request "/etc/nginx/conf.d/dhparam.pem" do
+  url "https://ssl-config.mozilla.org/ffdhe2048.txt"
+  mode "0644"
+  not_if "test -f /etc/nginx/conf.d/dhparam.pem"
+end
+
 template "/etc/nginx/conf.d/ssl.conf" do
   action :create
   source "templates/nginx-ssl.conf.erb"
